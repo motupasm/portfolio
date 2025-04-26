f33e7a04d0147372ce2c40066c416ce0d0167390
unknown <motupams@gmail.com>
2025-04-26 18:55:47 +0200

    first commit

---
+++ myscript.js
@@ -0,0 +1,10 @@
+function hidemenu(){
+    var menu = document.querySelector(".menu_items")
+    menu.style.display = "none"
+
+}
+function showmenu(){
+    var menu = document.querySelector(".menu_items")
+    menu.style.display = "flex"
+
+}
